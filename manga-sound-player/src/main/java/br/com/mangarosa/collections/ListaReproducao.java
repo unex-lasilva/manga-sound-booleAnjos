d11430e94885131257b0b9abdeaa4abd29a37b09
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-04-20 00:12:39 -0300

    Declaração de variaveis globais e classe construtora

---
+++ manga-sound-player/src/main/java/br/com/mangarosa/collections/ListaReproducao.java
@@ -0,0 +1,16 @@
+package br.com.mangarosa.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaReproducao {
+    private String titulo;
+    List<ListaEncadeada> ListaEncadeada = new ArrayList<>();
+
+    public ListaReproducao(String titulo) {
+
+    }
+
+    public void addMusi
+
+}
