add6d332d4bb6ec51cce1f007d45f68ffe79865a
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-04-20 01:12:09 -0300

    Estrutura organizacional dos metodos

---
+++ manga-sound-player/src/main/java/br/com/mangarosa/collections/MangaController.java
@@ -0,0 +1,52 @@
+package br.com.mangarosa.collections;
+
+public class MangaController {
+    ListaEncadeada repositorMusica;
+    ListaEncadeada listasReproducao;
+    ListaEncadeada artistas;
+    ReprodutorLista reprodutorLista;
+
+    public void adicionarMusica(String titulo, String path, String nomeArtista) {
+
+    }
+    public void CriarListaReproducao(String titulo) {
+
+    }
+    public void excluirListaReproducao(String titulo) {
+
+    }
+    public void adicionarMusicaListaReproducao(String tituloMusica, String tituloLista) {
+
+    }
+    public void adicionarMusicaListaReproducaoEmPosicao(String tituloMusica, String tituloLista, int posicao) {
+
+    }
+    public void removerMusicaListaReproducao(String tituloMusica, String tituloLista) {
+
+    }
+    public void removerMusicaListaReproducaoEmPosicao(String tituloLista, int posicao) {
+
+    }
+    public void reproduzirListaDeReproducao(String tituloLista) {
+
+    }
+    public void pausarMusica() {
+
+    }
+    public void executarMusica() {
+
+    }
+    public void voltarMusica() {
+
+    }
+    public void passarMusica() {
+
+    }
+    public void reiniciarMusica() {
+
+    }
+    public void pararLista() {
+
+    }
+
+}
