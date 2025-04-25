a89ee209409b499d7598527ff4a26c37fc11fa90
Jadson Sobrinho <102986608+Jadson-Sobrinho@users.noreply.github.com>
2025-04-25 19:58:27 -0300

    Chore(Musica.java)
    Comentarios para alteração do codigo

--- manga-sound-player/src/main/java/br/com/mangarosa/collections/Musica.java
+++ manga-sound-player/src/main/java/br/com/mangarosa/collections/Musica.java
@@ -1,11 +1,15 @@
 package br.com.mangarosa.collections;
 public class Musica {
+    
+    //Seria bom seguir as nomeclaturas do diagrama de classe para ficar padrão para outros da equipe
     private String nome;
     private String caminhoArquivo;
     private int duracaoSegundos;  // Em segundos
     private String genero;
+    // E o artista?
 
-
+    //Faltou o construtor default
+    //Quando tem um contrutor declarado explicitamentre, precisa declarar o construtor de defaoult explicitamente tbm
     public Musica(String nome, String caminhoArquivo, int duracaoSegundos, String genero) {
         this.nome = nome;
         this.caminhoArquivo = caminhoArquivo;
@@ -14,6 +18,8 @@ public class Musica {
     }
 
 
+    //Como vai atribuir os valores para as variaives? 
+    
     public String getNome() {
         return nome;
     }
@@ -30,7 +36,7 @@ public class Musica {
         return genero;
     }
 
-
+    //Acredito que não precise desta função, mas tudo bem
     @Override
     public String toString() {
         return nome + " (" + genero + ") - " + duracaoSegundos + "s";
